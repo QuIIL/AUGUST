ec352c35efc15907abefec6e74e97b32b0d999f8
Nguyen Quoc Khang <86041093+EnalisUs@users.noreply.github.com>
2026-01-16 11:50:07 +0900

    update

--- scripts/stage_1.sh
+++ scripts/stage_1.sh
@@ -1,7 +1,7 @@
 export WANDB_MODE=online
 export CUDA_VISIBLE_DEVICES=
 
-accelerate launch --config_file ./august/configs/configs.yaml ./august/trainers/stage_0.py" \
+accelerate launch --config_file ./august/configs/configs.yaml ./august/trainers/stage_1.py" \
     --jsonl ./august/data/CMCUJB_CAP_train.jsonl \
     --epochs 40 --batch_size 16 --lr 1e-3 \
     --gradient_accumulation_steps 1 \
